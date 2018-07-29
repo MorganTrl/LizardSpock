2d7ff0bd920f997e831175b4b558c5f401b839a7
Kateya <tirel.cynthia@gmail.com>
2018-07-29 04:31:57 +0200

    base du jeu en javascript

--- lizardspock.js
+++
@@ -1,9 +0,0 @@
-var weapon = prompt("Choisisez votre armes entre : Pierre, Papier, Ciseaux, Lézard ou Spock");
-var weaponList =  ['Pierre', 'Papier', 'Ciseaux', 'Lézard', 'Spock'];
-
-function randomSelect() {
-    var randomNb = Math.floor(Math.random(0,(weaponList.length - 1)));
-    console.log var randomWeapon = weaponList[randomNb]; 
-}
-
-randomSelect();
